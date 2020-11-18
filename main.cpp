ca67f36a268268cb112c3ec1e8dad0e2679b96a8
CheetoDustNeo <misterrolypoly@gmail.com>
2020-11-18 02:04:56 -0600

    added catchV2 test macro

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
+#define CATCH_CONFIG_RUNNER
+#include "catch.hpp"
 #include "mainwindow.h"
-
 #include <QApplication>
 #include "boardwidget.h"
 
 
 int main(int argc, char *argv[])
 {
+
+    int result = Catch::Session().run(argc,argv);
     QApplication a(argc, argv);
     BoardWidget b;
     b.show();
